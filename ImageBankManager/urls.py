2958c5647565f2589cce80b7a317cc01d7e6fd2f
Diogo Toporcov <diogotoporcov@gmail.com>
2025-11-14 07:18:27 -0300

    feat: integrate drf-spectacular for API schema generation
    - Added `drf-spectacular` and `drf-spectacular-sidecar` to requirements
    - Added spectacular URLs to project URL configuration
    - Registered `drf_spectacular` and `drf_spectacular_sidecar` in INSTALLED_APPS
    - Set default schema class in REST_FRAMEWORK settings
    - Added Spectacular settings for schema generation and UI

--- ImageBankManager/urls.py
+++ ImageBankManager/urls.py
@@ -16,9 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
