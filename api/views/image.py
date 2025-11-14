4d35e4c4e5c5e6854160fcc3fb186aaf59251458
Diogo Toporcov <diogotoporcov@gmail.com>
2025-11-14 18:46:12 -0300

    feat(api): add default ordering to all view sets

--- api/views/image.py
+++ api/views/image.py
@@ -1,8 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from api.models.image import Image
 from api.serializers.image import ImageSerializer
 
 
 class ImageViewSet(viewsets.ModelViewSet):
-    queryset = Image.objects.all().order_by("-created_at")
+    queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "created_at",
+        "updated_at",
+        "size_bytes",
+    ]
+    ordering = ["-created_at"]
--- api/views/user.py
+++ api/views/user.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from api.models.user import User
 from api.serializers.user import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by("-created_at")
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "created_at",
+        "updated_at",
+    ]
+    ordering = ["-created_at"]
