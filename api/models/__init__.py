b7905ce5a4871a52cb90298950caf7e918d6dece
Diogo Toporcov <diogotoporcov@gmail.com>
2025-11-13 21:42:20 -0300

    feat(api): remove Label model, serializer, view imports, and URL references

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,4 +3,3 @@ from .collection import Collection
 from .image import Image
 from .image_duplicate import ImageDuplicate
 from .image_fingerprint import ImageFingerprint
-from .label import Label
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,3 @@
 from .user import UserViewSet
-from .label import LabelViewSet
 from .collection import CollectionViewSet
 from .image import ImageViewSet
