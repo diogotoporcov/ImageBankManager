598611e26dc840e76ffe6b069526cad12126916d
Diogo Toporcov <diogotoporcov@gmail.com>
2025-11-14 03:49:59 -0300

    fix(api): remove ABC from HasLabelSerializerMixin

--- api/serializers/mixins/has_labels.py
+++ api/serializers/mixins/has_labels.py
@@ -1,10 +1,9 @@
-from abc import ABC
 from typing import List
 
 from rest_framework import serializers
 
 
-class HasLabelsSerializerMixin(ABC):
+class HasLabelsSerializerMixin:
     def validate_labels(self, labels: List[str]) -> List[str]:
         if labels and len(labels) != len(set(labels)):
             raise serializers.ValidationError("Labels must have unique values.")
